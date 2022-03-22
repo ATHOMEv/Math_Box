0020fc0cf3b3359eb1571e7cd086865f2d5ad7bb
ksjung <a112jung@naver.com>
2022-03-22 14:36:59 +0900

    merge

--- calculator.py
+++ calculator.py
@@ -3,10 +3,7 @@ def add(a, b):
   return a+b
 def subtract(a, b):
   return a-b
-<<<<<<< HEAD
 def devide(a,b):
   return a/b
-=======
 def muliply(a, b):
   return a*b
->>>>>>> f9ecc5200e5dd2305857ab73bea66f50150f39c0
